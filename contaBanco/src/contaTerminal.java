5df941c4149647d7ff5a5f766d8cdf6d7475502f
LitoSilva <devzelito37@gmail.com>
2024-04-27 23:46:59 -0300

    envior do progama pronto do desafio

--- contaBanco/src/contaTerminal.java
+++ contaBanco/src/contaTerminal.java
@@ -1,5 +1,33 @@
+/*
+ * Este desafio foi para criar um programa em java 
+ * @autor
+ */
+
+
+
+import java.util.Scanner;
+import java.util.Locale;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-       
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("***********************");
+        System.out.println("***** Banco World *****");
+        System.out.println("***********************\n");
+
+        System.out.println("Bem vindo vamos cria sua conta\n ");
+
+        System.out.println("Digite o numero da sua conta:");
+        int NumeroConta = sc.nextInt();
+        System.out.println("Digit o numero da Agencia:");
+        String agencia = sc.next();
+        System.out.println("Digite o nome do Cliente:");
+        String nomeCleinte = sc.next();
+        System.out.println("Digite o Saldo da conta:");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá,"+nomeCleinte+", Obrigado por criar uma conta em nosso banco, sua agencia e "+agencia+" conta "+NumeroConta+" e o seu saldo R$"+saldo+" ja esta desponivel para saque.");
+        
     }
 }
